f4560d04b74031df60a6f5b2a936c11500176a10
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-06-28 21:30:39 +0300

    write small code

---
+++ src/L1/PrintBigOrSmallUsingIf.py
@@ -0,0 +1,8 @@
+# Created by ali_h on 6/28/2026 at 9:29 PM
+# Source: docs/batool/lect1.pdf (page 4 of 7)
+
+x = eval(input("Enter the value: "))
+if x > 10:
+    print("big")
+else:
+    print("small")
