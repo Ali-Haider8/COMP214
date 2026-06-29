6042b5d2164f63c192d21acc1498db6ec3e41132
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-06-29 20:01:26 +0300

    write small code

---
+++ src/L1/CheckANameFromAList.py
@@ -0,0 +1,8 @@
+# Created by ali_h on 6/29/2026 at 7:50 PM
+# Source: docs/batool/lect1.pdf (page 6 of 7)
+arr = ["ali", "rana"]
+y = input("Enter your name: ")
+if y in arr:
+    print("exist")
+else:
+    print("not exist")
