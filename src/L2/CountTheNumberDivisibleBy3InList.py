e6f745038edf6849563742563cdb85afd41e5a38
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-07-01 18:17:36 +0300

    write small code

---
+++ src/L2/CountTheNumberDivisibleBy3InList.py
@@ -0,0 +1,10 @@
+# Created by ali_h on 7/1/2026 at 6:10 PM
+# Source: docs/batool/lect2.pdf (page 2 of 4)
+
+z = [1, 3, 5, 6, 8,9,10,12,15]
+c = 0
+for i in range(len(z)):
+    if z[i] % 3 == 0:
+        print(z[i])
+# c += 1
+# print("divisible by 3: ", c)
